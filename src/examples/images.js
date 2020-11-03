8328cef0c58ca50cbd477b7acf3fb4cbf5073079
Alex Panko <alexander.panko@gmail.com>
2020-11-03 21:26:52 +0100

    Images

---
+++ src/examples/images.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {graphql, useStaticQuery} from 'gatsby'
+import img from '../images/image1.jpg'
+import Image from 'gatsby-image'
+
+const getImages = graphql`
+  {
+    fixed: file(relativePath: { eq: "image3.jpg" }) {
+      childImageSharp {
+        fixed(width: 200, grayscale: true) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    fluid: file(relativePath: { eq: "image2.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+const Images = () => {
+    const data = useStaticQuery(getImages)
+    console.log(data)
+
+    return (
+      <section className="images">
+        <article className="single-image">
+          <h3>Basic image</h3>
+          <img src={img} width="100%" />
+        </article>
+        <article className="single-image">
+          <h3>Fixed image/blur</h3>
+          <Image fixed={data.fixed.childImageSharp.fixed} />
+        </article>
+        <article className="single-image">
+          <h3>Fluid image/svg</h3>
+          <Image fluid={data.fluid.childImageSharp.fluid} />
+          <div className="small">
+            <Image fluid={data.fluid.childImageSharp.fluid} />
+          </div>
+        </article>
+      </section>
+    )
+}
+
+export default Images
